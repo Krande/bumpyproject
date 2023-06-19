06e0b10fe698fa1c0df4dbc68676de7e3d6bd2be
krande <kristoffer_andersen@outlook.com>
2023-06-19 13:38:43 +0200

    basic cleanup of classes that should not be classes

--- src/bumpyproject/docker_helper.py
+++ src/bumpyproject/docker_helper.py
@@ -6,123 +6,121 @@ from azure.containerregistry import ContainerRegistryClient
 from azure.identity import ClientSecretCredential
 
 from bumpyproject import env_vars as env
-from bumpyproject.bumper import BumpHelper
-from bumpyproject.project import Project
+from bumpyproject import bumper
+from bumpyproject import project
+
+colors = {"black": 30, "red": 31, "green": 32, "yellow": 33, "blue": 34, "magenta": 35, "cyan": 36, "white": 37}
 
 
-class DockerHelper:
-    colors = {"black": 30, "red": 31, "green": 32, "yellow": 33, "blue": 34, "magenta": 35, "cyan": 36, "white": 37}
+def bump_acr_docker_image(context_dir, dockerfile_name, build, push, use_native_client):
+    if env.ACR_NAME is None:
+        raise ValueError("ACR_NAME environment variable is not set")
 
-    @staticmethod
-    def bump_acr_docker_image(context_dir, dockerfile_name, build, push, use_native_client):
-        if env.ACR_NAME is None:
-            raise ValueError("ACR_NAME environment variable is not set")
+    if env.ACR_REPO_NAME is None:
+        raise ValueError("repository argument is not set")
 
-        if env.ACR_REPO_NAME is None:
-            raise ValueError("repository argument is not set")
+    if isinstance(context_dir, str):
+        context_dir = pathlib.Path(context_dir).resolve().absolute()
 
-        if isinstance(context_dir, str):
-            context_dir = pathlib.Path(context_dir).resolve().absolute()
+    pyproject_version = project.get_pyproject_version()
+    current_docker_image_version = get_latest_tagged_image()
+    if not bumper.is_newer(current_docker_image_version, pyproject_version):
+        raise ValueError(f"New version {pyproject_version} is not newer than {current_docker_image_version}")
 
-        pyproject_version = Project.get_pyproject_version()
-        current_docker_image_version = DockerHelper.get_latest_tagged_image()
-        if not BumpHelper.is_newer(current_docker_image_version, pyproject_version):
-            raise ValueError(f"New version {pyproject_version} is not newer than {current_docker_image_version}")
+    # Build the Docker image
+    tagged_name = f"{env.ACR_NAME}.azurecr.io/{env.ACR_REPO_NAME}:{pyproject_version}"
 
-        # Build the Docker image
-        tagged_name = f"{env.ACR_NAME}.azurecr.io/{env.ACR_REPO_NAME}:{pyproject_version}"
+    if build or push:
+        build(context_dir, dockerfile_name, tagged_name)
 
-        if build or push:
-            DockerHelper.build(context_dir, dockerfile_name, tagged_name)
+    # Push the Docker image to Azure Container Registry
+    if push:
+        push(repo=tagged_name, use_native_client=use_native_client)
 
-        # Push the Docker image to Azure Container Registry
-        if push:
-            DockerHelper.push(repo=tagged_name, use_native_client=use_native_client)
 
-    @staticmethod
-    def colorize_text(color, text):
-        cm = DockerHelper.colors.get(color)
-        return f"\033[{cm}m" + text + "\033[0m"
+def colorize_text(color, text):
+    cm = colors.get(color)
+    return f"\033[{cm}m" + text + "\033[0m"
 
-    @staticmethod
-    def print_logs(build_logs, color="green"):
-        while True:
-            try:
-                json_output = build_logs.__next__()
-                status = json_output.get("status", None)
-                stream = json_output.get("stream", None)
-                if stream is not None:
-                    print(DockerHelper.colorize_text(color, stream.strip()))
-                elif status is not None:
-                    id_str = json_output.get("id", "")
-                    prog_str = json_output.get("progress", "").strip()
-                    prog_detail = json_output.get("progressDetail", "")
-                    if isinstance(prog_detail, dict) and len(prog_detail.keys()) == 0:
-                        prog_detail = ""
 
-                    state_message = f"{id_str}: {status} {prog_str} {prog_detail}"
-                    print(DockerHelper.colorize_text(color, state_message))
-                elif len(json_output.keys()) > 0:
-                    print(json_output)
-            except StopIteration:
-                break
-            except ValueError:
-                print("Error parsing output from docker image build: %s" % json_output)
+def print_logs(build_logs, color="green"):
+    while True:
+        try:
+            json_output = build_logs.__next__()
+            status = json_output.get("status", None)
+            stream = json_output.get("stream", None)
+            if stream is not None:
+                print(colorize_text(color, stream.strip()))
+            elif status is not None:
+                id_str = json_output.get("id", "")
+                prog_str = json_output.get("progress", "").strip()
+                prog_detail = json_output.get("progressDetail", "")
+                if isinstance(prog_detail, dict) and len(prog_detail.keys()) == 0:
+                    prog_detail = ""
 
-    @staticmethod
-    def build(context_dir, dockerfile_name, tag):
-        print(f'Building docker image with tag "{tag}"...')
-        DockerHelper.print_logs(
-            docker.APIClient().build(
-                path=str(context_dir),
-                tag=tag,
-                dockerfile=str(context_dir / dockerfile_name),
+                state_message = f"{id_str}: {status} {prog_str} {prog_detail}"
+                print(colorize_text(color, state_message))
+            elif len(json_output.keys()) > 0:
+                print(json_output)
+        except StopIteration:
+            break
+        except ValueError:
+            print("Error parsing output from docker image build: %s" % json_output)
+
+
+def build(context_dir, dockerfile_name, tag):
+    print(f'Building docker image with tag "{tag}"...')
+    print_logs(
+        docker.APIClient().build(
+            path=str(context_dir),
+            tag=tag,
+            dockerfile=str(context_dir / dockerfile_name),
+            decode=True,
+        )
+    )
+    print("Docker image build complete.")
+
+
+def push(repo, use_native_client=False):
+    print(f'Pushing docker image with tag "{repo}"...')
+    if use_native_client:
+        subprocess.run(
+            [
+                "docker",
+                "push",
+                repo,
+            ]
+        )
+    else:
+        print_logs(
+            docker.APIClient().push(
+                repository=repo,
                 decode=True,
+                auth_config={"username": env.ACR_CLIENT_ID, "password": env.ACR_CLIENT_SECRET},
+                stream=True,
             )
         )
-        print("Docker image build complete.")
+    print("Docker image push complete.")
 
-    @staticmethod
-    def push(repo, use_native_client=False):
-        print(f'Pushing docker image with tag "{repo}"...')
-        if use_native_client:
-            subprocess.run(
-                [
-                    "docker",
-                    "push",
-                    repo,
-                ]
-            )
-        else:
-            DockerHelper.print_logs(
-                docker.APIClient().push(
-                    repository=repo,
-                    decode=True,
-                    auth_config={"username": env.ACR_CLIENT_ID, "password": env.ACR_CLIENT_SECRET},
-                    stream=True,
-                )
-            )
-        print("Docker image push complete.")
 
-    @staticmethod
-    def get_latest_tagged_image():
-        if env.ACR_REPO_NAME is None:
-            raise ValueError("ACR_REPO_NAME environment variable is not set")
+def get_latest_tagged_image():
+    if env.ACR_REPO_NAME is None:
+        raise ValueError("ACR_REPO_NAME environment variable is not set")
 
-        # Get the service principal credentials
-        credential = ClientSecretCredential(
-            tenant_id=env.AZ_TENANT_ID, client_id=env.ACR_CLIENT_ID, client_secret=env.ACR_CLIENT_SECRET
-        )
+    # Get the service principal credentials
+    credential = ClientSecretCredential(
+        tenant_id=env.AZ_TENANT_ID, client_id=env.ACR_CLIENT_ID, client_secret=env.ACR_CLIENT_SECRET
+    )
 
-        # Create a ContainerRegistryClient object
-        acr_client = ContainerRegistryClient(
-            endpoint=f"https://{env.ACR_NAME}.azurecr.io",
-            credential=credential,
-            audience="https://management.azure.com",
-        )
+    # Create a ContainerRegistryClient object
+    acr_client = ContainerRegistryClient(
+        endpoint=f"https://{env.ACR_NAME}.azurecr.io",
+        credential=credential,
+        audience="https://management.azure.com",
+    )
 
-        # Get the list of tags for repository
-        tags = list(acr_client.list_tag_properties(repository=env.ACR_REPO_NAME))
-        latest_tag = tags[-1].name
-        print(f"The latest tagged image of {env.ACR_REPO_NAME} is: {latest_tag}")
-        return latest_tag
+    # Get the list of tags for repository
+    tags = list(acr_client.list_tag_properties(repository=env.ACR_REPO_NAME))
+    latest_tag = tags[-1].name
+    print(f"The latest tagged image of {env.ACR_REPO_NAME} is: {latest_tag}")
+    return latest_tag
--- src/bumpyproject/git_helper.py
+++ src/bumpyproject/git_helper.py
@@ -1,13 +1,13 @@
 import pathlib
 
 import git
-import semver
 import tomlkit
+
+from bumpyproject import bumper
 from bumpyproject import env_vars as env
-from bumpyproject.bumper import BumpHelper, BumpLevelSizeError
-from bumpyproject.project import Project
-from bumpyproject.versions import BumpLevel
+from bumpyproject import project
 from bumpyproject.log_utils import logger
+from bumpyproject.versions import BumpLevel
 
 
 class DirtyRepoError(Exception):
@@ -15,26 +15,56 @@ class DirtyRepoError(Exception):
 
 
 class GitHelper:
-    @staticmethod
-    def check_git_history():
-        current_version = Project.get_pyproject_version()
-        git_old_version = GitHelper.get_pyproject_toml_version_from_latest_pushed_commit()
+    def __init__(self, git_root_dir=None):
+        if git_root_dir is None:
+            git_root_dir = env.GIT_ROOT_DIR
+
+        if isinstance(git_root_dir, str):
+            git_root_dir = pathlib.Path(git_root_dir)
+
+        git_root_dir = git_root_dir.resolve().absolute()
+
+        self._git_root_dir = git_root_dir
+        self._git_repo = git.Repo(git_root_dir)
+
+        remotes = list(self._git_repo.remotes)
+        if len(remotes) == 1:
+            self._remote = remotes[0]
+        elif len(remotes) > 1:
+            raise ValueError("Currently only one git remote is supported.")
+        else:
+            logger.warning("No git remote found. This may cause issues.")
+
+    @property
+    def repo_root_dir(self) -> pathlib.Path:
+        return self._git_root_dir
+
+    @property
+    def git_repo(self) -> git.Repo:
+        return self._git_repo
+
+    @property
+    def git_remote(self) -> git.Remote:
+        return self._remote
+
+    def check_git_history(self):
+        current_version = project.get_pyproject_version()
+        git_old_version = self.get_pyproject_toml_version_from_latest_pushed_commit()
         if git_old_version is None or current_version == git_old_version:
             return None
 
-        delta = BumpHelper.get_bump_delta(git_old_version, current_version)
+        delta = bumper.get_bump_delta(git_old_version, current_version)
 
         # Catch bumping more than one level
         non_ones_or_zeros = [i for i, x in enumerate(delta) if x != 0 and x != 1]
         if len(non_ones_or_zeros) > 0:
-            raise BumpLevelSizeError(
+            raise bumper.BumpLevelSizeError(
                 f"Cannot bump {current_version=} from {git_old_version=} because it is not a single level bump"
             )
 
-    @staticmethod
-    def get_pyproject_toml_version_from_latest_pushed_commit():
+    def get_pyproject_toml_version_from_latest_pushed_commit(self):
         # Initialize the repo object
-        remote = GitHelper.get_git_remote()
+        remote = self.git_remote
 
         # If there are no pushed commits return None
         if len(remote.refs) == 0:
@@ -43,9 +73,9 @@ class GitHelper:
         # Get the last pushed commit
         latest_pushed_commit = remote.refs[0].commit
 
-        pytoml = Project.pyproject_toml_path()
-        root_dir = GitHelper.get_git_root_dir()
-        toml_rel = pytoml.relative_to(root_dir)
+        pytoml = project.get_pyproject_toml_path_from_env()
+
+        toml_rel = pytoml.relative_to(self._git_root_dir)
 
         # Get the pyproject.toml file from both commits
         try:
@@ -59,49 +89,40 @@ class GitHelper:
 
         # Get the version from the file
         version = toml_data["project"]["version"]
-        version = Project.make_py_ver_semver(version)
+        version = project.make_py_ver_semver(version)
         return version
 
-    @staticmethod
-    def check_git_state():
-        curr_repo = GitHelper.get_git_repo()
-        if curr_repo.is_dirty():
+    def check_git_state(self):
+        curr_repo = self._git_repo
+        if curr_repo.is_dirty(self):
             raise DirtyRepoError("There are uncommitted changes!")
 
-    @staticmethod
-    def get_latest_tag() -> str:
-        curr_repo = GitHelper.get_git_repo()
-        tags = list(curr_repo.tags)
+    def get_latest_tag(self) -> str:
+        tags = list(self.git_repo.tags)
         return tags[-1].name
 
-    @staticmethod
-    def commit_and_tag(old_version, new_version):
+    def commit_and_tag(self, old_version, new_version):
         commit_message = f"bump {old_version} --> {new_version}"
-        curr_repo = GitHelper.get_git_repo()
+        curr_repo = self.git_repo
         curr_repo.git.config("user.email", env.GIT_USER_EMAIL)
         curr_repo.git.config("user.name", env.GIT_USER)
         curr_repo.git.execute(["git", "commit", "-am", commit_message])
         curr_repo.git.execute(["git", "tag", "-a", new_version, "-m", commit_message])
 
-    @staticmethod
-    def push():
-        curr_repo = GitHelper.get_git_repo()
-        remote = GitHelper.get_git_remote()
-        remote.push(refspec=f"{curr_repo.active_branch}:{curr_repo.active_branch}")
+    def push(self):
+        curr_repo = self.git_repo
+        self.git_remote.push(refspec=f"{curr_repo.active_branch}:{curr_repo.active_branch}")
         curr_repo.git.push()
         curr_repo.git.push("--tags")
 
-    @staticmethod
-    def create_branch(branch_name, push=True):
-        curr_repo = GitHelper.get_git_repo()
+    def create_branch(self, branch_name, push=True):
+        curr_repo = self.git_repo
         curr_repo.git.checkout("-b", branch_name)
         if push:
             curr_repo.git.push("--set-upstream", "origin", branch_name)
 
-    @staticmethod
-    def get_bump_level_from_commit() -> BumpLevel:
-        curr_repo = GitHelper.get_git_repo()
-        commits = list(curr_repo.iter_commits(max_count=1))
+    def get_bump_level_from_commit(self) -> BumpLevel:
+        commits = list(self.git_repo.iter_commits(max_count=1))
         latest_commit = commits[0]
         msg = latest_commit.message
         for level in BumpLevel:
@@ -112,28 +133,3 @@ class GitHelper:
             return BumpLevel.PRE_RELEASE
 
         raise ValueError(f'No bump level found in commit message "{msg}"')
-
-    @staticmethod
-    def get_git_root_dir():
-        git_root_dir = env.GIT_ROOT_DIR
-        if isinstance(git_root_dir, str):
-            git_root_dir = pathlib.Path(git_root_dir)
-
-        git_root_dir = git_root_dir.resolve().absolute()
-        return git_root_dir
-
-    @staticmethod
-    def get_git_repo() -> git.Repo:
-        git_root_dir = GitHelper.get_git_root_dir()
-        return git.Repo(git_root_dir)
-
-    @staticmethod
-    def get_git_remote() -> git.Remote | None:
-        repo = GitHelper.get_git_repo()
-        remotes = list(repo.remotes)
-        if len(remotes) == 0:
-            return None
-        elif len(remotes) > 1:
-            raise ValueError("Currently only one git remote is supported.")
-
-        return remotes[0]
