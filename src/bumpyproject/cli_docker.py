39939ce5e2717a03c7e133ccd7df203fd42dc252
krande <kristoffer_andersen@outlook.com>
2023-06-21 14:26:02 +0200

    minor changes

--- src/bumpyproject/cli_docker.py
+++ src/bumpyproject/cli_docker.py
@@ -15,7 +15,7 @@ def main():
     )
     parser.add_argument("--pyproject-toml", help="Path to pyproject.toml", default=env.PYPROJECT_TOML)
     parser.add_argument("--package-json", help="Path to package.json", default=env.PKG_JSON)
-
+    parser.add_argument("--acr-repo-name", help="Azure ACR repository name", default=env.ACR_REPO_NAME)
     parser.add_argument("--context-dir", required=True, help="Context directory for .")
     parser.add_argument("--dockerfile", required=True, help="Dockerfile absolute or relative path (to context dir).")
     parser.add_argument("--push", action="store_true", help="Docker push.")
@@ -24,12 +24,16 @@ def main():
 
     args = parser.parse_args()
 
+    if args.acr_repo_name:
+        env.ACR_REPO_NAME = args.acr_repo_name
+
     project = Project(
         pyproject_toml=args.pyproject_toml,
         package_json=args.package_json,
         docker_context=args.context_dir,
         dockerfile=args.dockerfile,
     )
+
     docker_helper.bump_acr_docker_image(project, args.build, args.push, args.use_native_client)
 
 
